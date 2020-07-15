433d9b49db5609b28794402138b3837afed1648d
guangheUltimate <18903378092@163.com>
2020-07-15 10:45:26 +0800

    多媒体管理系统-dao层编写-暂存

--- webapp/src/main/java/io/guangsoft/media/service/ICommonService.java
+++ webapp/src/main/java/io/guangsoft/media/service/ICommonService.java
@@ -1,23 +1,19 @@
 package io.guangsoft.media.service;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
-import io.guangsoft.common.query.data.Page;
-import io.guangsoft.common.query.data.Queryable;
-import io.guangsoft.media.data.DuplicateValid;
 
 import java.util.List;
 
 public interface ICommonService<T> extends IService<T> {
 
-     io.guangsoft.common.query.data.Page<T> list(Queryable queryable);
+    public Page<T> listPage(QueryWrapper<T> wrapper);
 
-     Page<T> list(Queryable queryable, QueryWrapper<T> wrapper);
+    public Page<T> listPage(QueryWrapper<T> wrapper, Page<T> page);
 
-     List<T> listWithNoPage(Queryable queryable);
+    List<T> list();
 
-     List<T> listWithNoPage(Queryable queryable, QueryWrapper<T> wrapper);
+    List<T> list(QueryWrapper<T> wrapper);
 
-     Boolean doValid(DuplicateValid duplicateValid, QueryWrapper<T> wrapper);
 }
--- webapp/src/main/java/io/guangsoft/media/service/impl/CommonServiceImpl.java
+++ webapp/src/main/java/io/guangsoft/media/service/impl/CommonServiceImpl.java
@@ -2,17 +2,10 @@ package io.guangsoft.media.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import io.guangsoft.common.query.data.Page;
-import io.guangsoft.common.query.data.PageImpl;
-import io.guangsoft.common.query.data.Pageable;
-import io.guangsoft.common.query.data.Queryable;
-import io.guangsoft.media.data.DuplicateValid;
 import io.guangsoft.media.service.ICommonService;
-import io.guangsoft.media.data.QueryToWrapper;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -20,74 +13,26 @@ import java.util.List;
 public class CommonServiceImpl<M extends BaseMapper<T>, T> extends ServiceImpl<M, T> implements ICommonService<T> {
 
     @Override
-    public Page<T> list(Queryable queryable) {
-        QueryWrapper<T> QueryWrapper = new QueryWrapper<T>();
-        return list(queryable, QueryWrapper);
+    public Page<T> listPage(QueryWrapper<T> wrapper) {
+        Page<T> page = new Page<>(1, 10);
+        return listPage(wrapper, page);
     }
 
     @Override
-    public Page<T> list(Queryable queryable, QueryWrapper<T> wrapper) {
-        QueryToWrapper<T> queryToWrapper = new QueryToWrapper<T>();
-        queryToWrapper.parseCondition(wrapper, queryable);
-        // 排序问题
-        queryToWrapper.parseSort(wrapper, queryable);
-        Pageable pageable = queryable.getPageable();
-        IPage<T> page = new com.baomidou.mybatisplus.extension.plugins.pagination.Page<>(pageable.getPageNumber(), pageable.getPageSize());
-        IPage<T> content = super.page(page, wrapper);
-        return new PageImpl<T>(content.getRecords(), queryable.getPageable(), content.getTotal());
+    public Page<T> listPage(QueryWrapper<T> wrapper, Page<T> page) {
+        baseMapper.selectPage(page, wrapper);
+        return page;
     }
 
     @Override
-    public List<T> listWithNoPage(Queryable queryable) {
-        QueryWrapper<T> QueryWrapper = new QueryWrapper<T>();
-        return listWithNoPage(queryable, QueryWrapper);
+    public List<T> list() {
+        QueryWrapper<T> queryWrapper = new QueryWrapper<T>();
+        return list(queryWrapper);
     }
 
     @Override
-    public List<T> listWithNoPage(Queryable queryable, QueryWrapper<T> wrapper) {
-        QueryToWrapper<T> queryToWrapper = new QueryToWrapper<T>();
-
-        queryToWrapper.parseCondition(wrapper, queryable);
-        // 排序问题
-        queryToWrapper.parseSort(wrapper, queryable);
-        List<T> content = list(wrapper);
-        return content;
-    }
-
-    @Override
-    public Boolean doValid(DuplicateValid duplicateValid, QueryWrapper<T> wrapper) {
-        Boolean valid = Boolean.FALSE;
-        String queryType = duplicateValid.getQueryType();
-        if (StringUtils.isEmpty(queryType)) {
-            queryType = "table";
-        }
-        if (queryType.equals("table")) {
-            valid = validTable(duplicateValid, wrapper);
-        }
-        return valid;
-    }
-
-    private Boolean validTable(DuplicateValid duplicateValid, QueryWrapper<T> wrapper) {
-        Integer num = null;
-        String extendName = duplicateValid.getExtendName();
-        String extendParam = duplicateValid.getExtendParam();
-        if (!StringUtils.isEmpty(extendParam)) {
-            // [2].编辑页面校验
-            wrapper.eq(duplicateValid.getName(), duplicateValid.getParam()).ne(extendName, extendParam);
-            num = super.count(wrapper);
-        } else {
-            // [1].添加页面校验
-            wrapper.eq(duplicateValid.getName(), duplicateValid.getParam());
-            num = super.count(wrapper);
-        }
-
-        if (num == null || num == 0) {
-            // 该值可用
-            return true;
-        } else {
-            // 该值不可用
-            return false;
-        }
+    public List<T> list(QueryWrapper<T> wrapper) {
+        return baseMapper.selectList(wrapper);
     }
 
 }
